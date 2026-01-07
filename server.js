a81a0c0f14527453c568b5387b1a1383647b2b53
4drea4 <bakerand@outlook.com>
2026-01-07 12:49:10 -0600

    setting up project directions

---
+++ server.js
@@ -0,0 +1,12 @@
+//import express
+
+//import path
+
+//create an instance
+
+
+// Create a route handler for GET requests to the root URL (/). When this route is requested, it should send the index.html file from your public directory.
+
+// Create another route handler for GET requests to /contact. This should send the contact.html file.
+
+// Start the server and have it listen on your chosen port. When it starts, it should log a message to the console, like Server is running on port 3000.
