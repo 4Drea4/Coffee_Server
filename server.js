3fbf8adda662549782a16bb7df645617d06cbaa9
4drea4 <bakerand@outlook.com>
2026-01-07 13:10:06 -0600

    okay so updated the path for contact and index about to try it out

--- server.js
+++ server.js
@@ -7,14 +7,12 @@ const port = 3000; //set the port
 app.use(express.static(path.join(__dirname,'public')));
 // Create a route handler for GET requests to the root URL (/). When this route is requested, it should send the index.html file from your public directory.
 app.get('/', (req,res)=> {
-    res.sent
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
-
-
 
 // Create another route handler for GET requests to /contact. This should send the contact.html file.
 app.get('/contact', (req,res)=> {
-    res.sent
+    res.sendFile(path.join (__dirname, 'public', 'contact.html'));
 })
 // Start the server and have it listen on your chosen port. When it starts, it should log a message to the console, like Server is running on port 3000.
 app.listen(port, () => {
