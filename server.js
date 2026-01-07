3b66e61bfc3b6f8afe2abe387b28c6bd996940c9
4drea4 <bakerand@outlook.com>
2026-01-07 12:54:08 -0600

    added middleware and cleaned up my existing code a bit

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-//import express
+const express = require('express'); //import express
+const app = express(); //create an instance
 
-//import path
+const port = 3000; //set the port
 
-//create an instance
-
+//middleware
+app.arguments(express.static(path.join(__dirname,'public')))
 
 // Create a route handler for GET requests to the root URL (/). When this route is requested, it should send the index.html file from your public directory.
+app.get('/', (req,res)=> {
+    res.sent
+})
+
+
 
 // Create another route handler for GET requests to /contact. This should send the contact.html file.
-
+app.get('/', (req,res)=> {
+    res.sent
+})
 // Start the server and have it listen on your chosen port. When it starts, it should log a message to the console, like Server is running on port 3000.
